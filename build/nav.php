45b88674b5faa05808da184827cf718cb902f2c9
Artx999 <54852665+Artx999@users.noreply.github.com>
2020-09-11 17:44:14 +0200

    Initial commit

---
+++ build/nav.php
@@ -0,0 +1,7 @@
+<nav>
+    <img src="">
+    <ul>
+        <li class="navItem"><a href="https://localhost/IT2/">Hjem</a></li>
+        <li class="navItem"><a href="<?php $rootPath ?>projects.php">Prosjekter</a></li>
+    </ul>
+</nav>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php $rootPath = "" ?>
+<?php require "build/head.php" ?>
+<script>setTitle("Hjem")</script>
+<?php require "build/nav.php" ?>
+<header>
+</header>
+<code>
+    #main {color: yellow}
+</code>
