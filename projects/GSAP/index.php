3556412de1a3b73808963868fb629fbc0f7af6b7
Artx999 <54852665+Artx999@users.noreply.github.com>
2020-09-15 13:24:35 +0200

    Update index.php

--- projects/GSAP/index.php
+++ projects/GSAP/index.php
@@ -1,5 +1,5 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script>
-<script src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/findShapeIndex.js "></script>
+<script src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/MorphSVGPlugin3.min.js"></script>
 <style>
     body {
         overflow: hidden;
@@ -20,16 +20,14 @@
 <img src="svg2.svg" alt="svg 2" id="svg2">
 <svg id="star" height="200" xmlns="http://www.w3.org/2000/svg">
     <polygon class="star1" points="100,10 40,180 190,60 10,60 160,180" fill="yellow"/>
-</svg>
-<svg id="lol" width="300" height="200">
-    <polygon class="star2" points="100,10 40,198 190,78 10,78 160,198"
-             style="fill: #b30086;stroke:#000000;stroke-width:5;fill-rule:evenodd;" />
+    <polygon class="star2" points="200,10 250,190 160,210" style="fill:lime;stroke:purple;stroke-width:1" />
 </svg>
 
 
 <script>
     let tline = gsap.timeline()
     tline.to(".star1", {
+        duration: 1,
         morphSVG: ".star2"
     })
     tline.to("img", {
