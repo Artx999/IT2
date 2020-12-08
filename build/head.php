61354bfb2896ca62ebb7c135f90174871f3a22ad
Artx999 <54852665+Artx999@users.noreply.github.com>
2020-12-08 21:10:47 +0100

    Added DOM-site

--- build/head.php
+++ build/head.php
@@ -5,5 +5,6 @@
     <title></title>
     <link rel="stylesheet" type="text/css" href="<?php print $rootPath ?>style.css">
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v3.0.0/css/line.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="<?php print $rootPath ?>script.js"></script>
 </head>
